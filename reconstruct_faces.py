3e4654ba66345ca5391d514020bc0f20151c9a95
Bernardo Janko Goncalves Biesseck <biesseck@gmail.com>
2023-12-14 00:15:29 -0300

    reconstruct_faces.py: sort files paths

--- reconstruct_faces.py
+++ reconstruct_faces.py
@@ -73,7 +73,7 @@ def run_hrn(args):
 
     # names = sorted([name for name in os.listdir(args.input_root) if '.jpg' in name or '.png' in name or '.jpeg' in name or '.PNG' in name or '.JPG' in name or '.JPEG' in name])
     print(f'\nSearching image files in \'{args.input_root}\'...')
-    names = find_image_files(args.input_root)
+    names = find_image_files(args.input_root) 
     # for i, name in enumerate(names):
     #     print(i, '- name:', name)
     # print('len(names):', len(names))
