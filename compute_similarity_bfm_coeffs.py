1bbdd1cfbbb15510d19a79c7a4cb4f3fbf6c3d18
Bernardo Biesseck <biesseck@gmail.com>
2024-09-04 18:18:39 -0300

    Add 'compute_similarity_bfm_coeffs.py'

---
+++ compute_similarity_bfm_coeffs.py
@@ -0,0 +1,118 @@
+import os, sys
+import numpy as np
+import argparse
+
+from util.load_mats import LoadBFM09, LoadExpBasis
+
+from scipy.io import loadmat
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--bfm_basis', type=str, default='/datasets2/bjgbiesseck/BaselFaceModel/PublicMM1/01_MorphableModel.mat', help='')
+    parser.add_argument('--exp_basis', type=str, default='/datasets2/bjgbiesseck/microsoft_Deep3DFaceReconstruction/Exp_Pca.bin', help='')
+
+    parser.add_argument('--sample0', type=str, default='/home/bjgbiesseck/GitHub/BOVIFOCR_HRN_3D_face_reconstruction/assets/results/KDEF_results/AF02/AF02AFFL/AF02AFFL_coeffs.npy', help='')
+    parser.add_argument('--sample1', type=str, default='/home/bjgbiesseck/GitHub/BOVIFOCR_HRN_3D_face_reconstruction/assets/results/KDEF_results/AF03/AF03AFFL/AF03AFFL_coeffs.npy', help='')
+
+    parser.add_argument('--normalize', action="store_true")
+
+
+    args = parser.parse_args()
+    return args
+
+
+def split_coeff(coeffs):
+    id_coeffs = coeffs[:, :80]         # face identity    (80)
+    exp_coeffs = coeffs[:, 80: 144]    # face expression  (64)
+    tex_coeffs = coeffs[:, 144: 224]   # texture          (80)
+    angles = coeffs[:, 224: 227]       # face rotation    ( 3)
+    gammas = coeffs[:, 227: 254]       # gammas           (27)
+    translations = coeffs[:, 254:]     # face translation ( 3)
+    return {
+        'id': id_coeffs,
+        'exp': exp_coeffs,
+        'tex': tex_coeffs,
+        'angle': angles,
+        'gamma': gammas,
+        'trans': translations
+    }
+
+
+def euclidean_distance(array1, array2, normalize=False):
+    array1 = np.array(array1)
+    array2 = np.array(array2)
+    if normalize:
+        array1 = array1 / np.linalg.norm(array1)
+        array2 = array2 / np.linalg.norm(array2)
+    distance = np.sqrt(np.sum((array1 - array2) ** 2))
+    return distance
+
+
+def cosine_similarity(vec1, vec2, normalize=False):
+    vec1 = np.array(vec1)
+    vec2 = np.array(vec2)
+    if normalize:
+        vec1 = vec1 / np.linalg.norm(vec1)
+        vec2 = vec2 / np.linalg.norm(vec2)
+
+    dot_product = np.dot(np.squeeze(vec1), np.squeeze(vec2))
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    
+    if norm_vec1 == 0 or norm_vec2 == 0:
+        return 0.0
+    
+    return dot_product / (norm_vec1 * norm_vec2)
+
+
+
+def rescale_coeffs(bfm_coeffs_split, model_bfm_exp):
+    bfm_coeffs_split['id']  *= np.squeeze(model_bfm_exp['idEV'])
+    bfm_coeffs_split['exp'] *= np.squeeze(model_bfm_exp['exEV'])
+    bfm_coeffs_split['tex'] *= np.squeeze(model_bfm_exp['texEV'])
+    return bfm_coeffs_split
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # dict_keys(['meanshape', 'meantex', 'idBase', 'idEV', 'exBase', 'exEV', 'texBase', 'texEV', 'tri', 'point_buf', 'tri_mask2', 'keypoints', 'frontmask2_idx', 'skinmask'])
+    model_bfm_exp = LoadBFM09(bfm_folder=os.path.dirname(args.bfm_basis), exp_folder=os.path.dirname(args.exp_basis))
+    # print('model_bfm_exp.keys()', model_bfm_exp.keys())
+    # print("model_bfm_exp['idBase'].shape:", model_bfm_exp['idBase'].shape)
+    # print("model_bfm_exp['idEV'].shape:", model_bfm_exp['idEV'].shape)
+    # print("model_bfm_exp['exBase'].shape:", model_bfm_exp['exBase'].shape)
+    # print("model_bfm_exp['exEV'].shape:", model_bfm_exp['exEV'].shape)
+    # print("model_bfm_exp['texBase'].shape:", model_bfm_exp['texBase'].shape)
+    # print("model_bfm_exp['texEV'].shape:", model_bfm_exp['texEV'].shape)
+    # sys.exit(0)
+
+    bfm0 = np.load(args.sample0)
+    bfm1 = np.load(args.sample1)
+    # print('bfm0.shape:', bfm0.shape)
+    # print('bfm1.shape:', bfm1.shape)
+
+    # 'id', 'exp', 'tex', 'angle', 'gamma', 'trans'
+    bfm0_split = split_coeff(bfm0)
+    bfm1_split = split_coeff(bfm1)
+    # print('bfm0_split:', bfm0_split)
+
+    bfm0_split = rescale_coeffs(bfm0_split, model_bfm_exp)
+    bfm1_split = rescale_coeffs(bfm1_split, model_bfm_exp)
+
+    # id_eucl_dist = euclidean_distance(bfm0_split['id'], bfm1_split['id'], normalize=args.normalize)
+    # exp_eucl_dist = euclidean_distance(bfm0_split['exp'], bfm1_split['exp'], normalize=args.normalize)
+    # pose_eucl_dist = euclidean_distance(bfm0_split['angle'], bfm1_split['angle'], normalize=args.normalize)
+    # print('id_eucl_dist:   %.2f' % id_eucl_dist)
+    # print('exp_eucl_dist:  %.2f' % exp_eucl_dist)
+    # print('pose_eucl_dist: %.2f' % pose_eucl_dist)
+    # print('------------')
+
+    id_cossim   = cosine_similarity(bfm0_split['id'], bfm1_split['id'], normalize=args.normalize)
+    exp_cossim  = cosine_similarity(bfm0_split['exp'], bfm1_split['exp'], normalize=args.normalize)
+    pose_cossim = cosine_similarity(bfm0_split['angle'], bfm1_split['angle'], normalize=args.normalize)
+    print('id_cossim:   %.2f' % id_cossim)
+    print('exp_cossim:  %.2f' % exp_cossim)
+    print('pose_cossim: %.2f' % pose_cossim)
--- util/load_mats.py
+++ util/load_mats.py
@@ -98,6 +98,95 @@ def transferBFM09(bfm_folder='BFM'):
             'tri': tri, 'point_buf': point_buf, 'tri_mask2': tri_mask2, 'keypoints': keypoints, 'frontmask2_idx': frontmask2_idx, 'skinmask': skinmask})
 
 
+
+
+
+# Bernardo
+def LoadBFM09(bfm_folder='BFM', exp_folder='EXP'):
+    print('Loading BFM09 basis...')
+    original_BFM = loadmat(osp.join(bfm_folder, '01_MorphableModel.mat'))
+    shapePC = original_BFM['shapePC']  # shape basis, 160470*199
+    shapeEV = original_BFM['shapeEV']  # corresponding eigen value, 199*1
+    shapeMU = original_BFM['shapeMU']  # mean face, 160470*1
+    texPC = original_BFM['texPC']  # texture basis, 160470*199
+    texEV = original_BFM['texEV']  # eigen value, 199*1
+    texMU = original_BFM['texMU']  # mean texture, 160470*1
+
+    print('Loading Expression basis...')
+    # expPC, expEV = LoadExpBasis()           # original
+    expPC, expEV = LoadExpBasis(exp_folder)   # Bernardo
+
+    # transfer BFM09 to our face model
+
+    idBase = shapePC*np.reshape(shapeEV, [-1, 199])
+    idBase = idBase/1e5  # unify the scale to decimeter
+    idBase = idBase[:, :80]  # use only first 80 basis
+    shapeEV = shapeEV[:80, :]/1e5    # Bernardo
+
+    exBase = expPC*np.reshape(expEV, [-1, 79])
+    exBase = exBase/1e5  # unify the scale to decimeter
+    exBase = exBase[:, :64]  # use only first 64 basis
+    expEV = expEV[:64]/1e5           # Bernardo
+
+    texBase = texPC*np.reshape(texEV, [-1, 199])
+    texBase = texBase[:, :80]  # use only first 80 basis
+    texEV = texEV[:80, :]/1e5        # Bernardo
+
+    # our face model is cropped along face landmarks and contains only 35709 vertex.
+    # original BFM09 contains 53490 vertex, and expression basis provided by Guo et al. contains 53215 vertex.
+    # thus we select corresponding vertex to get our face model.
+
+    # index_exp = loadmat(osp.join(bfm_folder, 'BFM_front_idx.mat'))   # original
+    index_exp = loadmat(osp.join(exp_folder, 'BFM_front_idx.mat'))     # Bernardo
+    index_exp = index_exp['idx'].astype(np.int32) - 1  # starts from 0 (to 53215)
+
+    # index_shape = loadmat(osp.join(bfm_folder, 'BFM_exp_idx.mat'))   # original
+    index_shape = loadmat(osp.join(exp_folder, 'BFM_exp_idx.mat'))     # Bernardo
+    index_shape = index_shape['trimIndex'].astype(
+        np.int32) - 1  # starts from 0 (to 53490)
+    index_shape = index_shape[index_exp]
+
+    idBase = np.reshape(idBase, [-1, 3, 80])
+    idBase = idBase[index_shape, :, :]
+    idBase = np.reshape(idBase, [-1, 80])
+
+    texBase = np.reshape(texBase, [-1, 3, 80])
+    texBase = texBase[index_shape, :, :]
+    texBase = np.reshape(texBase, [-1, 80])
+
+    exBase = np.reshape(exBase, [-1, 3, 64])
+    exBase = exBase[index_exp, :, :]
+    exBase = np.reshape(exBase, [-1, 64])
+
+    meanshape = np.reshape(shapeMU, [-1, 3])/1e5
+    meanshape = meanshape[index_shape, :]
+    meanshape = np.reshape(meanshape, [1, -1])
+
+    meantex = np.reshape(texMU, [-1, 3])
+    meantex = meantex[index_shape, :]
+    meantex = np.reshape(meantex, [1, -1])
+
+    # other info contains triangles, region used for computing photometric loss,
+    # region used for skin texture regularization, and 68 landmarks index etc.
+    # other_info = loadmat(osp.join(bfm_folder, 'facemodel_info.mat'))   # original
+    other_info = loadmat(osp.join(exp_folder, 'facemodel_info.mat'))     # Bernardo
+    frontmask2_idx = other_info['frontmask2_idx']
+    skinmask = other_info['skinmask']
+    keypoints = other_info['keypoints']
+    point_buf = other_info['point_buf']
+    tri = other_info['tri']
+    tri_mask2 = other_info['tri_mask2']
+
+    # save our face model
+    # savemat(osp.join(bfm_folder, 'BFM_model_front.mat'), {'meanshape': meanshape, 'meantex': meantex, 'idBase': idBase, 'exBase': exBase, 'texBase': texBase,
+    #         'tri': tri, 'point_buf': point_buf, 'tri_mask2': tri_mask2, 'keypoints': keypoints, 'frontmask2_idx': frontmask2_idx, 'skinmask': skinmask})
+    return {'meanshape': meanshape, 'meantex': meantex, 'idBase': idBase, 'idEV':shapeEV, 'exBase': exBase, 'exEV':expEV, 'texBase': texBase, 'texEV': texEV,
+            'tri': tri, 'point_buf': point_buf, 'tri_mask2': tri_mask2, 'keypoints': keypoints, 'frontmask2_idx': frontmask2_idx, 'skinmask': skinmask}
+
+
+
+
+
 # load landmarks for standard face, which is used for image preprocessing
 def load_lm3d(bfm_folder):
 
